4ac8faeb58a54fd607d30aa6c171acf377e51f7c
Raghu Venkatesh <raghuv@andrew.cmu.edu>
2018-09-21 23:21:27 -0400

    added files and Handy

---
+++ videoCapture1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from random import randint
+from handy import *
+
+hist = captureHistogram(0)
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    ret, frame, countours, defects = detectHand(frame, hist, sketchContours = True, computeDefects = True)
+    fingertips = extractFingertips(defects, countours, 50, right = True)
+    plot(frame, fingertips)
+    frame = cv2.resize(frame, (0,0), fx=2, fy = 2)
+    
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
