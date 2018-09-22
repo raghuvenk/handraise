c298ee8aca8da4d4254425aef42e036b6432b08a
Raghu Venkatesh <raghuv@andrew.cmu.edu>
2018-09-22 13:11:34 -0400

    rectangle of face

--- videoCapture2.py
+++ videoCapture2.py
@@ -3,12 +3,15 @@ import cv2
 from random import randint
 from handy import *
 
+raiseHeight = 160
+faceWidth = 90
+
 hist = captureHistogram(0)
-print(hist)
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
-raiseHeight = 200
-capWidth = cap.get(3)
+capWidth  = int(cap.get(3))
+capHeight = int(cap.get(4))
+
 
 while(True):
     # Capture frame-by-frame
@@ -18,18 +21,22 @@ while(True):
     plot(frame, fingertips)
 
     #drawing a line above which the hand counts as raised
-    frame = cv2.line(frame, (0, 200), (int(capWidth - 1), 200), (255, 255, 0), 1)
-    if type(countours) == np.ndarray
-        m = countours.mean()
-        if countours.mean() < raiseHeight:
+    frame = cv2.line(frame, (0, raiseHeight), (int(capWidth - 1), raiseHeight), (255, 255, 0), 1)
+    if type(countours) == np.ndarray:
+        h = countours[0][0][1]
+        w = int(countours[0][0][0])
+        if h < raiseHeight:
             cv2.putText(frame, "Raised!", (50, 350), font, 4, (0, 0, 255), 2, cv2.LINE_AA)
+            
+            frame = cv2.rectangle(frame, (max(w - faceWidth, 0), capHeight - 1), (w, raiseHeight - 1), (0, 0, 255), 2)
+            
     
-    #frame = cv2.resize(frame, (0,0), fx = 2, fy = 2)
+    frame = cv2.resize(frame, (0,0), fx = 2, fy = 2)
     
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            break
 
 # When everything done, release the capture
 cap.release()
