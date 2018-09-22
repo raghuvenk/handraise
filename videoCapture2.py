58b135727db9db02251891c45a000e148cb21904
Raghu Venkatesh <raghuv@andrew.cmu.edu>
2018-09-22 17:19:45 -0400

    spacing

--- videoCapture2.py
+++ videoCapture2.py
@@ -40,7 +40,7 @@ while(True):
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        break
 
 # When everything done, release the capture
 cap.release()
