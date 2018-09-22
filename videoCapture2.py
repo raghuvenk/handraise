1c7a1821109669c81e5a5dd920597dc1b178c2c3
Raghu Venkatesh <raghuv@andrew.cmu.edu>
2018-09-22 11:36:17 -0400

    worked on Horizontal Line trigger for Handy

--- videoCapture2.py
+++ videoCapture2.py
@@ -7,6 +7,8 @@ hist = captureHistogram(0)
 print(hist)
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
+raiseHeight = 200
+capWidth = cap.get(3)
 
 while(True):
     # Capture frame-by-frame
@@ -14,6 +16,13 @@ while(True):
     ret, frame, countours, defects = detectHand(frame, hist, sketchContours = True, computeDefects = True)
     fingertips = extractFingertips(defects, countours, 50, right = True)
     plot(frame, fingertips)
+
+    #drawing a line above which the hand counts as raised
+    frame = cv2.line(frame, (0, 200), (int(capWidth - 1), 200), (255, 255, 0), 1)
+    if type(countours) == np.ndarray
+        m = countours.mean()
+        if countours.mean() < raiseHeight:
+            cv2.putText(frame, "Raised!", (50, 350), font, 4, (0, 0, 255), 2, cv2.LINE_AA)
     
     #frame = cv2.resize(frame, (0,0), fx = 2, fy = 2)
     
